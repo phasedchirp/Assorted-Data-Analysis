a9885f4c931a8cd50216609842500f11a5a19bc3
phasedchirp <sean.martin@nyu.edu>
2016-04-09 09:36:15 -0700

    updated bootstrap version, but currently broken.

--- online-bootstrap.py
+++ online-bootstrap.py
@@ -14,7 +14,7 @@ def onlineMeanVar(xs):
         delta = x - m
         m += delta/n
         M2 += delta**2
-    
+
     if n < 2:
         return (np.nan,np.nan)
     else:
@@ -24,23 +24,26 @@ def onlineMeanVar(xs):
 # Hanley & MacGibbon (2006) (http://www.ncbi.nlm.nih.gov/pubmed/16730851)
 # and
 # http://www.unofficialgoogledatascience.com/2015/08/an-introduction-to-poisson-bootstrap_26.html
-#def onlineMeanVarBoot(xs,reps):
-    #ns = np.zeros(reps,dtype=np.int)
-    #ms = np.zeros(reps)
-    #M2s = np.zeros(reps)
-    
-    #for x in xs:
-        #counts = poisson(1,reps)
-        #ns += counts
-        #deltas = counts .* x
-        #ms += deltas/ns
-        #M2s += deltas .**2
-    
-    #if stuff:
-        #pass
-    #else:
-        #pass
-        
+def onlineMeanVarBoot(xs,reps):
+    ns = np.zeros(reps,dtype=np.int)
+    ms = np.zeros(reps)
+    M2s = np.zeros(reps)
+
+    for x in xs:
+        counts = poisson(1,reps)
+        ns += counts
+        deltas = (counts * x) - ms
+        for i in range(len(ms)):
+            if ns[i] > 0:
+                ms[i] += deltas[i] / ns[i]
+        # ms += deltas / ns
+        M2s += deltas **2
+
+    if np.min(ns) < 2:
+        return np.nan
+    else:
+        return M2s / ns
+
 
 test = rand(500)
 
