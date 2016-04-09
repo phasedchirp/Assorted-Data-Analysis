c1635ddb02fd0ae598076cd8f0efaa8c17615e70
phasedchirp <sean.martin@nyu.edu>
2016-04-09 10:38:30 -0700

    working but slow version of bootstrap for variance. Seems to be working but need to do test comparisons.

--- online-bootstrap.py
+++ online-bootstrap.py
@@ -24,6 +24,7 @@ def onlineMeanVar(xs):
 # Hanley & MacGibbon (2006) (http://www.ncbi.nlm.nih.gov/pubmed/16730851)
 # and
 # http://www.unofficialgoogledatascience.com/2015/08/an-introduction-to-poisson-bootstrap_26.html
+# see also: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Weighted_incremental_algorithm
 def onlineMeanVarBoot(xs,reps):
     ns = np.zeros(reps,dtype=np.int)
     ms = np.zeros(reps)
@@ -31,13 +32,17 @@ def onlineMeanVarBoot(xs,reps):
 
     for x in xs:
         counts = poisson(1,reps)
-        ns += counts
-        deltas = (counts * x) - ms
+        temp = ns + counts
+        deltas = x - ms
+        Rs = np.zeros(reps)
         for i in range(len(ms)):
             if ns[i] > 0:
-                ms[i] += deltas[i] / ns[i]
-        # ms += deltas / ns
-        M2s += deltas **2
+                Rs[i] = deltas[i] * counts[i] / temp[i]
+            else:
+                Rs[i]  = 0
+        ms += Rs
+        M2s += ns * deltas * Rs
+        ns = temp
 
     if np.min(ns) < 2:
         return np.nan
