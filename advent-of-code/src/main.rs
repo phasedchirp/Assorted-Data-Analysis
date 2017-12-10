66da5d860dabbef15e4a9cda1c18f1f70f26e9c6
phasedchirp <phasedchirp2718@gmail.com>
2017-12-10 12:36:22 -0800

    some cleanup on part_1 for day 3

--- advent-of-code/src/main.rs
+++ advent-of-code/src/main.rs
@@ -1,12 +1,12 @@
-fn part_1(input: u32) -> u32 {
+use std::collections::HashMap;
+
+fn part_1(input: i32) -> i32 {
     if input == 1 {
         0
     } else {
-        let inner = (0u32..).map(|x| (2*x+1, (2*x + 1).pow(2)))
-            .take_while(|x| x.1 < input).last().unwrap();
-        let outer = (inner.0+2, (inner.0+2).pow(2));
-        let pos = (outer.1 - input) % (outer.0 - 1); // distance from cor
-        // println!("{}: {:?}, {}, {}", input, outer, pos, outer.0/2);
+        let outer = (0i32..).map(|x| (2*x+1, (2*x + 1).pow(2)))
+            .skip_while(|x| x.1 < input).nth(0).unwrap();
+        let pos = (outer.1 - input) % (outer.0 - 1); // distance from corner
         match outer.0/2 >= pos {
             true => outer.0 - 1 - pos,
             false => outer.0 - 1 - (pos/2)
@@ -14,9 +14,37 @@ fn part_1(input: u32) -> u32 {
     }
 }
 
-fn part_2(input: &str) -> u32 {
-    unimplemented!()
-}
+// #[derive(Debug)]
+// struct Pt {
+//     x: i32,
+//     y: i32,
+//     val: i32
+// }
+//
+// impl Pt {
+//     fn new(x: i32, y: i32) -> Pt {
+//         let np = Pt{x: x, y: y, val: 0}
+//     }
+//     fn get_neighbors(&self) -> Vec<(i32,i32)> {
+//         vec![(1,1),(0,1),(1,-1),(1,0),(0,-1),(-1,1),(-1,0),(-1,-1)]
+//             .iter().map(|(x,y)| (x+self.x, y+self.y))
+//             .collect()
+//     }
+//
+//     fn set_val(&mut self, world: HashMap<(i32,i32),Pt>) {
+//         self.get_neighbors().iter()
+//             .fold(0,|acc,x|{
+//                 match world.get(x) {
+//                     Some(z) => acc + z.val,
+//                     None    => acc
+//             }
+//         })
+//     }
+// }
+//
+// fn part_2(input: &str) -> i32 {
+//     (1i32..).fold(HashMap::new(),|x|)
+// }
 
 fn main() {
     assert!(part_1(1) == 0);
